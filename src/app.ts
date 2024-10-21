7da32b06c466ef3a6460ee5065004115b578c3cb
kyand38 <kyand2024@gmail.com>
2024-10-20 20:53:47 -0500

    added the viewAllDepartments, viewAllRoles, and viewAllEmployees functions

--- src/app.ts
+++ src/app.ts
@@ -17,21 +17,21 @@ inquirer.prompt({
       ]
 }).then (async answer => {
     switch (answer.choose) {
-      // case 'View All Departments':
-      //   viewAllDepartments();
-      //   break;
-      // case 'View All Roles':
-      //   viewAllRoles();
-      //   break;
+      case 'View All Departments':
+        viewAllDepartments();
+        break;
+      case 'View All Roles':
+        viewAllRoles();
+        break;
       case 'View All Employees':
         await viewAllEmployees();
         break;
       // case 'Add a Department':
       //   addDepartment();
       //   break;
-      // case 'Add a Role':
-      //   addRole();
-      //   break;
+      case 'Add a Role':
+        addRole('test title 2', 150000, 3);
+        break;
       // case 'Add an Employee':
       //   addEmployee();
       //   break;
@@ -44,30 +44,76 @@ inquirer.prompt({
   });
 
 
-// async function viewAllDepartments(){
-//     const result = pool.query()
-//     console.log(result);
-// }
-// async function viewAllRoles(){
-//     const client = await db.connect();
-//     const result =client.query('')
-//     console.log(result);
-// }
+  async function viewAllDepartments(){
+    const client = await pool.connect();
+    const result = await client.query('')
+    // TODO: handle errors
+    console.table(result.rows);
+    client.release();
+ }
+ async function viewAllRoles(){
+  const client = await pool.connect();
+  const result = await client.query('SELECT * FROM role')
+  // TODO: handle errors
+  console.table(result.rows);
+  client.release();
+ }
+// function to view all employees on a table
 async function viewAllEmployees(){
     const client = await pool.connect();
     const result = await client.query('SELECT first_name, last_name FROM employee')
-    console.log(result);
+    // TODO: handle errors
+    console.table(result.rows);
+    client.release();
+
 }
+
 // async function addDepartment(){
 //     const client = await db.connect();
 //     const result =client.query('')
 //     console.log(result);
 // }
-// async function addRole(){
-//     const client = await db.connect();
-//     const result =client.query('')
-//     console.log(result);
-// }
+
+async function addRole(){
+  //get title from user
+
+  //get slaary from user
+
+  // get department form user
+  // show user list of departments
+
+  //pass answers into addRoleToDb
+}
+
+async function addRoleToDb(title: string, salary: number, department_id: number){
+  const client = await pool.connect();
+  try{ 
+    // TODO: parameterize the query 
+    await client.query('BEGIN');
+
+    const query = 'INSERT INTO role (title, salary, department_id)VALUES ($1, $2, $3);';
+    const values = [title, salary, department_id]; // Pass your parameters in an array
+    const result = await client.query(query, values);
+
+    await client.query('COMMIT');
+
+    console.table(result.rows);
+  }catch(err) {
+    await client.query('ROLLBACK');
+    console.error('Error inserting role:', err);
+  }finally{
+    if (client){
+      client.release()
+    }
+  }
+  }
+// inquirer.prompt([
+//   name: 'NewRole',
+//   message: 'New Role Title?',
+//   type: 'input',
+
+// ]);
+
 // async function addEmployee(){
 //     const client = await db.connect();
 //     const result =client.query('')
